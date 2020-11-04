c94a3bcf349d8b2e6d5dddcfdda44bc66d298167
scleveland <Sean Cleveland>
2020-11-04 12:33:23 -0700

    add production build files

---
+++ build/precache-manifest.f0819594527b6189cb53150188a51090.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62060e149567f8b7ee31a4188a0c0b48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0234ea0ab903d8979496",
+    "url": "/static/css/main.638630f6.chunk.css"
+  },
+  {
+    "revision": "341422361926f9a6002b",
+    "url": "/static/js/2.c49e4184.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.c49e4184.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0234ea0ab903d8979496",
+    "url": "/static/js/main.983d43f3.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
